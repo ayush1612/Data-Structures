9758c4158186c736ef82b3ae6260c4c2dc8eb888
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2018-12-11 10:08:48 +0000

    Binary Search

---
+++ binary_search.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#define size 10
+void main()
+{
+	int arr[size];
+	printf("Enter size elements in the array\n");
+	
+	int i,j;
+	for(i=0;i<size;i++)
+		scanf("%d",&arr[i]);
+		
+	int search;
+	printf("Enter the element you want to search\n");
+	scanf("%d",&search);
+	
+	//sorting the array
+	
+	for(i=0;i<size;i++)
+	{
+		for(j=0;j<size-i-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	
+	//binary searching the element 
+	
+	int left=0,right=9,found=0;
+	while(left<=right)
+	{
+		int mid=(left+right)/2;
+		
+		if(arr[mid]==search)
+		{
+			found=1;
+			break;
+		}
+		else if(arr[mid]>search)
+		right=mid-1;
+		else
+		left=mid+1;
+	}
+	
+	if(found==0)
+	printf("Element not present\n");
+	else
+	printf("Element present in the array\n");
+}
+
